a88ad65a38bd73b3477f62f002d46ad5fe83e0cc
Rob Gerstenberger <rgerstenberger@gmail.com>
2013-04-16 09:10:04 -0700

    basic structure

--- Assignment 1/application.js
+++ Assignment 1/application.js
@@ -2,26 +2,100 @@ window.Application = {};
 
 Application.View = Backbone.View.extend({
   render: function() {
-    var context = this.model ? this.model.attributes : {},
+    var context = this.collection ? this.collection : {},
         output = this.options.template(context);
     this.$el.html(output);
   }
 });
 
 $(function() {
-  var TV = Backbone.Model.extend({
+  var Television = Backbone.Model.extend({
     defaults: {
       title: "",
       description: "", //the example JSON only had name but the page requires title and description
       price: "",
       listPrice: "",
       image: "",//is there a placeholder image appropriate for this?
-      size: "" //should proper defaults for numbers be 0?
+      size: "", //should proper defaults for numbers be 0?
+      type: ""
     }
   });
+
+  // var TelevisionView = Backbone.View.extend({
+
+  // });
+
+  var Televisions = Backbone.Collection.extend({
+    model: Television
+  });
+
+
+  var televisions = new Televisions([{
+    "title": "Samsung 32-inch LCD HTDTV",
+      "description": "Samsung 32C120U 32-Inch 720p 60Hz LCD HDTV (Black)", //the example JSON only had name but the page requires title and description
+      "price": 390.99,      
+      "image": "images/tv1.png",//is there a placeholder image appropriate for this?
+      "size": 32, //should proper defaults for numbers be 0?
+      "type": "LCD"
+    },
+    {
+    "title": "Samsung 32-inch LCD HTDTV",
+      "description": "Samsung 32C120U 32-Inch 720p 60Hz LCD HDTV (Black)", //the example JSON only had name but the page requires title and description
+      "price": 390.99,      
+      "image": "images/tv1.png",//is there a placeholder image appropriate for this?
+      "size": 32, //should proper defaults for numbers be 0?
+      "type": "LCD"
+    },
+    {
+    "title": "Samsung 32-inch LCD HTDTV",
+      "description": "Samsung 32C120U 32-Inch 720p 60Hz LCD HDTV (Black)", //the example JSON only had name but the page requires title and description
+      "price": 390.99,      
+      "image": "images/tv1.png",//is there a placeholder image appropriate for this?
+      "size": 32, //should proper defaults for numbers be 0?
+      "type": "LCD"
+    },
+    {
+    "title": "Samsung 32-inch LCD HTDTV",
+      "description": "Samsung 32C120U 32-Inch 720p 60Hz LCD HDTV (Black)", //the example JSON only had name but the page requires title and description
+      "price": 390.99,      
+      "image": "images/tv1.png",//is there a placeholder image appropriate for this?
+      "size": 32, //should proper defaults for numbers be 0?
+      "type": "LCD"
+    },
+    {
+    "title": "Samsung 32-inch LCD HTDTV",
+      "description": "Samsung 32C120U 32-Inch 720p 60Hz LCD HDTV (Black)", //the example JSON only had name but the page requires title and description
+      "price": 390.99,      
+      "image": "images/tv1.png",//is there a placeholder image appropriate for this?
+      "size": 32, //should proper defaults for numbers be 0?
+      "type": "LCD"
+    },
+    {
+    "title": "Samsung 32-inch LCD HTDTV",
+      "description": "Samsung 32C120U 32-Inch 720p 60Hz LCD HDTV (Black)", //the example JSON only had name but the page requires title and description
+      "price": 390.99,      
+      "image": "images/tv1.png",//is there a placeholder image appropriate for this?
+      "size": 32, //should proper defaults for numbers be 0?
+      "type": "LCD"
+    },
+    {
+    "title": "Samsung 32-inch LCD HTDTV",
+      "description": "Samsung 32C120U 32-Inch 720p 60Hz LCD HDTV (Black)", //the example JSON only had name but the page requires title and description
+      "price": 390.99,      
+      "image": "images/tv1.png",//is there a placeholder image appropriate for this?
+      "size": 32, //should proper defaults for numbers be 0?
+      "type": "LCD"
+    }
+]);
+
+  //populate the collection from json file or televisions.fetch();?
+
   var indexView = new Application.View({
     template: Handlebars.templates['index'],
-    model: model
+    collection: televisions,
+    events: {
+      //all filter events should trigger the same refresh/filter data function?
+    }
   });
   indexView.render();
   $('body').append(indexView.el);
